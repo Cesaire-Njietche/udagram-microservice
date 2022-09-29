eb0c8c63cfb8a506c8471858c10ab1c8a14b8660
Cesaire-Njietche <zecdnc2014@gmail.com>
2022-09-29 12:27:05 +0100

    nginx updated

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
+  // apiHost: "http://localhost:8080/api/v0",
   apiHost:
     "http://af9c68c48b6f449649f663e9f3328a4b-1536942546.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
